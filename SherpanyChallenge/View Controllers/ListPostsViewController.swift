314996694793106dfb951f18a390b85345cc2c40
Mikolaj Kalinowski <mikolaj.kalinowski@gmail.com>
2017-12-22 21:28:08 +0100

    Integrate ListPosts with FetchedResultsViewController

--- SherpanyChallenge/View Controllers/ListPostsViewController.swift
+++ SherpanyChallenge/View Controllers/ListPostsViewController.swift
@@ -6,11 +6,69 @@
 //  Copyright © 2017 Higher Order. All rights reserved.
 //
 
+import CoreData
 import UIKit
 
 class ListPostsViewController: UITableViewController {
+    private var controller: NSFetchedResultsController<Post>?
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Challenge Accepted!"
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.estimatedRowHeight = 80
+        tableView.rowHeight = UITableViewAutomaticDimension
+
+        controller = CoreDataService.shared.fetchedResultsController()
+        controller?.delegate = self
+        do {
+            try controller?.performFetch()
+        } catch {
+            fatalError("Failed to fetch entities: \(error)")
+        }
+    }
+}
+
+extension ListPostsViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return controller?.sections?.count ?? 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return controller?.sections?[section].numberOfObjects ?? 0
+    }
+
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        if let post = controller?.object(at: indexPath) {
+            cell.textLabel?.text = post.body
+        }
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView,
+                            titleForHeaderInSection section: Int) -> String? {
+        return controller?.sections?[section].name
+    }
+
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return controller?.sectionIndexTitles
+    }
+
+    override func tableView(_ tableView: UITableView,
+                            sectionForSectionIndexTitle title: String,
+                            at index: Int) -> Int {
+        return controller?.section(forSectionIndexTitle: title, at: index) ?? 0
+    }
+}
+
+extension ListPostsViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
     }
 }
