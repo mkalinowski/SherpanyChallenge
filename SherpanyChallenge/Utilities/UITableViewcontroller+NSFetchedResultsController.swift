3297861b78435bef938c99b92d34b3139f17b5d2
Mikolaj Kalinowski <mikolaj.kalinowski@gmail.com>
2017-12-26 23:13:25 +0100

    Extract NSFetchedResultsController conformance implementation

---
+++ SherpanyChallenge/Utilities/UITableViewcontroller+NSFetchedResultsController.swift
@@ -0,0 +1,61 @@
+//
+//  UITableViewcontroller+NSFetchedResultsController.swift
+//  SherpanyChallenge
+//
+//  Created by Mikolaj Kalinowski on 26/12/2017.
+//  Copyright © 2017 Higher Order. All rights reserved.
+//
+
+import CoreData
+import UIKit
+
+extension UITableViewController: NSFetchedResultsControllerDelegate {
+    public func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+
+    public func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+
+    public func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                           didChange sectionInfo: NSFetchedResultsSectionInfo,
+                           atSectionIndex sectionIndex: Int,
+                           for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            return
+        }
+    }
+
+    public func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                           didChange anObject: Any,
+                           at indexPath: IndexPath?,
+                           for type: NSFetchedResultsChangeType,
+                           newIndexPath: IndexPath?) {
+
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath
+                else { break }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath
+                else { break }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath
+                else { break }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let indexPath = indexPath,
+                let newIndexPath = newIndexPath
+                else { break }
+            tableView.reloadRows(at: [indexPath, newIndexPath], with: .automatic)
+        }
+    }
+}
